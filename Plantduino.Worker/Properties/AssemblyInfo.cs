b75a09a7bc0e674e8a6a2820d318bd283dd1e0e9
tom.davis <tom.davis@photomoose.com>
2015-05-12 07:47:32 +0100

    Renamed Worker project to Plantduino.Worker.

--- Plantduino.Worker/Properties/AssemblyInfo.cs
+++ Plantduino.Worker/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Worker")]
+[assembly: AssemblyTitle("Plantduino.Worker")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Worker")]
+[assembly: AssemblyCompany("Rumr Ltd.")]
+[assembly: AssemblyProduct("Plantduino")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
