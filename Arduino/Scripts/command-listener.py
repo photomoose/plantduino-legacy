4d8f02dc520c3d59e6033aa5207826c8958d4b20
tom.davis <tom.davis@photomoose.com>
2015-07-03 08:32:19 +0100

    Added command-listener script.

--- Arduino/Scripts/command-listener.py
+++ Arduino/Scripts/command-listener.py
@@ -12,10 +12,10 @@ from azure import WindowsAzureError
 from azure.servicebus import ServiceBusService, Rule
 from urllib2 import URLError, HTTPError
 
-LOG_FILENAME = './notifications.log'
+LOG_FILENAME = './command-listener.log'
 
 # Set up a specific logger with our desired output level
-logger = logging.getLogger('notifications')
+logger = logging.getLogger('command-listener')
 logger.setLevel(logging.DEBUG)
 
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -38,19 +38,11 @@ try:
 	                        shared_access_key_name=key_name,
 	                        shared_access_key_value=key_value)
 
-	sbs.create_subscription('notifications', 'ColdSpellNotifications_1')
-
-	coldSpells = {}
-
-	#rule = Rule()
-	#rule.filter_type = 'SqlFilter'
-	#rule.filter_type = "MessageType = 'ColdSpellEnteredNotification'"
-	#sbs.create_rule('notifications', 'ColdSpellNotifications_1', 'ColdSpellNotifications', rule)
-	#sbs.delete_rule('notifications', 'ColdSpellNotifications_1', DEFAULT_RULE_NAME)
+	sbs.create_subscription('commands', 'arduino')
 
 	while True:
 		try:
-			msg = sbs.receive_subscription_message('notifications', 'ColdSpellNotifications_1')
+			msg = sbs.receive_subscription_message('commands', 'arduino')
 		
 			if msg.body:
 				obj = json.loads(msg.body)
@@ -58,20 +50,14 @@ try:
 				logger.info('Received ' + msg.custom_properties['messagetype'])
 				logger.debug(obj)
 
-				if msg.custom_properties['messagetype'] == 'ColdSpellEnteredNotification':
-					coldSpells[obj['SensorId']] = True
-				elif msg.custom_properties['messagetype'] == 'ColdSpellLeftNotification':
-					coldSpells[obj['SensorId']] = False
-
-
-				if True in coldSpells.values():
-					urllib2.urlopen('http://localhost/mailbox/coldspellledon')
-					logger.info('Cold Spell LED: On')
+				if msg.custom_properties['messagetype'] == 'IrrigateCommand':
+					urllib2.urlopen('http://localhost/mailbox/irrigate')
+					logger.info('Irrigation: On')
+					msg.delete()
 				else:
-					urllib2.urlopen('http://localhost/mailbox/coldspellledoff')
-					logger.info('Cold Spell LED: Off')
+					logger.warn('Unrecognised message type: ' + msg.custom_properties['messagetype'])
+					msg.unlock()
 
-				msg.delete()
 		except (WindowsAzureError, URLError, HTTPError, IOError) as e:
 			logger.warn('Error: ' + traceback.format_exc())
 
